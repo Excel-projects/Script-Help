c844b295e22848aaafbb6cdfd423f3c36301110e
anthonyduguid <anthonyduguid@gmail.com>
2017-08-04 12:16:19 +1000

    Updated the database connection procedure and added download procedure for web

--- Scripts/Data.cs
+++ Scripts/Data.cs
@@ -29,10 +29,17 @@ namespace ScriptHelp.Scripts
 
 				if (!Directory.Exists(userFilePath)) Directory.CreateDirectory(userFilePath);
 
-				string sourceFilePath = Path.Combine(Properties.Settings.Default.App_PathDeploy, "Application Files", AssemblyInfo.Product + versionNumber, dataFolder, AssemblyInfo.Product + ".sdf.deploy");
+				Uri baseUrl = new Uri(Properties.Settings.Default.App_PathDeploy);
+				string relativeUrl = "Application Files/" + AssemblyInfo.Product + versionNumber + "/" + dataFolder + "/" + AssemblyInfo.Product + ".sdf.deploy";
+				Uri combined = new Uri(baseUrl, relativeUrl);
+				string sourceFilePath = combined.ToString();
+
+				//string sourceFilePath = Path.Combine(Properties.Settings.Default.App_PathDeploy, "Application Files", AssemblyInfo.Product + versionNumber, dataFolder, AssemblyInfo.Product + ".sdf.deploy");
 				string destFilePath = Path.Combine(userFilePath, AssemblyInfo.Product + ".sdf");
 				if (File.Exists(destFilePath)) File.Delete(destFilePath);
-				if (!File.Exists(destFilePath)) File.Copy(sourceFilePath, destFilePath);
+				Data.DownloadFile(sourceFilePath, destFilePath);
+
+				//if (!File.Exists(destFilePath)) File.Copy(sourceFilePath, destFilePath);  //use when deployed to a internal server
 
 				//if (File.GetLastWriteTime(destFilePath).CompareTo(File.GetLastWriteTime(sourceFilePath)) != 0)
 				//{
@@ -140,7 +147,7 @@ namespace ScriptHelp.Scripts
 			}
 
 		}
-
+		
 		/// <summary>
 		/// Creates the datatable for the graph data
 		/// </summary>
@@ -173,6 +180,64 @@ namespace ScriptHelp.Scripts
 
 			}
 
+		}
+
+		/// <summary>
+		/// Download a file from a web site
+		/// </summary>
+		/// <param name="sourceURL">web site source file</param>
+		/// <param name="destinationPath">local path</param>
+		public static void DownloadFile(string sourceURL, string destinationPath)
+		{
+			long fileSize = 0;
+			int bufferSize = 1024;
+			bufferSize *= 1000;
+			long existLen = 0;
+
+			try
+			{
+				System.IO.FileStream saveFileStream;
+				if (System.IO.File.Exists(destinationPath))
+				{
+					System.IO.FileInfo destinationFileInfo = new System.IO.FileInfo(destinationPath);
+					existLen = destinationFileInfo.Length;
+				}
+
+				if (existLen > 0)
+					saveFileStream = new System.IO.FileStream(destinationPath,
+															  System.IO.FileMode.Append,
+															  System.IO.FileAccess.Write,
+															  System.IO.FileShare.ReadWrite);
+				else
+					saveFileStream = new System.IO.FileStream(destinationPath,
+															  System.IO.FileMode.Create,
+															  System.IO.FileAccess.Write,
+															  System.IO.FileShare.ReadWrite);
+
+				System.Net.HttpWebRequest httpReq;
+				System.Net.HttpWebResponse httpRes;
+				httpReq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(sourceURL);
+				httpReq.AddRange((int)existLen);
+				System.IO.Stream resStream;
+				httpRes = (System.Net.HttpWebResponse)httpReq.GetResponse();
+				resStream = httpRes.GetResponseStream();
+
+				fileSize = httpRes.ContentLength;
+
+				int byteSize;
+				byte[] downBuffer = new byte[bufferSize];
+
+				while ((byteSize = resStream.Read(downBuffer, 0, downBuffer.Length)) > 0)
+				{
+					saveFileStream.Write(downBuffer, 0, byteSize);
+				}
+
+			}
+			catch (Exception ex)
+			{
+				ErrorHandler.DisplayMessage(ex);
+
+			}
 		}
 
 	}
