40e2bfe0a2105f820b2aca32076a05727cb46d1e
aduguid <anthonyduguid@gmail.com>
2017-10-30 17:07:10 +1100

    Updated table data taskpane

--- VB/TaskPane/TableData.Designer.vb
+++ VB/TaskPane/TableData.Designer.vb
@@ -22,8 +22,60 @@ Partial Class TableData
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.toolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.btnSave = New System.Windows.Forms.ToolStripButton()
+        Me.dgvList = New System.Windows.Forms.DataGridView()
+        Me.toolStrip1.SuspendLayout()
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'toolStrip1
+        '
+        Me.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.toolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnSave})
+        Me.toolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.toolStrip1.Name = "toolStrip1"
+        Me.toolStrip1.Size = New System.Drawing.Size(300, 25)
+        Me.toolStrip1.TabIndex = 4
+        Me.toolStrip1.Text = "toolStrip1"
+        '
+        'btnSave
+        '
+        Me.btnSave.Image = Global.ScriptHelp.My.Resources.Resources.Save
+        Me.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(51, 22)
+        Me.btnSave.Text = "Save"
+        Me.btnSave.ToolTipText = "Save changes"
+        '
+        'dgvList
+        '
+        Me.dgvList.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvList.Location = New System.Drawing.Point(0, 28)
+        Me.dgvList.Name = "dgvList"
+        Me.dgvList.Size = New System.Drawing.Size(300, 722)
+        Me.dgvList.TabIndex = 3
+        '
+        'TableData
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.toolStrip1)
+        Me.Controls.Add(Me.dgvList)
+        Me.Name = "TableData"
+        Me.Size = New System.Drawing.Size(300, 750)
+        Me.toolStrip1.ResumeLayout(False)
+        Me.toolStrip1.PerformLayout()
+        CType(Me.dgvList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Private WithEvents toolStrip1 As Windows.Forms.ToolStrip
+    Private WithEvents btnSave As Windows.Forms.ToolStripButton
+    Private WithEvents dgvList As Windows.Forms.DataGridView
 End Class
--- VB/TaskPane/TableData.vb
+++ VB/TaskPane/TableData.vb
@@ -1,3 +1,67 @@
-﻿Public Class TableData
+﻿Imports System.Windows.Forms
+Imports System.Data.SqlServerCe
+
+Public Class TableData
+
+    Public Sub New()
+        InitializeComponent()
+        Try
+            dgvList.AutoGenerateColumns = True
+            Dim tableName As String = Ribbon.AppVariables.TableName
+            Me.Text = "List of " + tableName
+            Select Case tableName
+                Case "TableAlias"
+                    'dgvList.DataSource = Data.TableAliasTable
+                    Exit Select
+                Case "DateFormat"
+                    'dgvList.DataSource = Data.DateFormatTable
+                    Exit Select
+            End Select
+
+            Me.dgvList.Columns(0).Width = dgvList.Width - 75
+
+        Catch ex As Exception
+            'ErrorHandler.DisplayMessage(ex)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            Me.Validate()
+            If dgvList.IsCurrentRowDirty OrElse dgvList.IsCurrentCellDirty Then
+                dgvList.CommitEdit(DataGridViewDataErrorContexts.Commit)
+                dgvList.EndEdit()
+            End If
+
+            Dim tableName As String = Ribbon.AppVariables.TableName
+            'string sql = "SELECT * FROM @tableName";
+            Dim sql As String = Convert.ToString("SELECT * FROM ") & tableName
+            Dim cn As New SqlCeConnection(Data.Connection())
+            Dim scb As SqlCeCommandBuilder = Nothing
+            Dim sda As New SqlCeDataAdapter(sql, cn)
+
+            sda.TableMappings.Add("Table", tableName)
+            scb = New SqlCeCommandBuilder(sda)
+            Select Case tableName
+                Case "TableAlias"
+                    sda.Update(Data.TableAliasTable)
+                    Data.CreateTableAliasTable()
+                    Exit Select
+                Case "DateFormat"
+                    sda.Update(Data.DateFormatTable)
+                    Data.CreateDateFormatTable()
+                    Exit Select
+            End Select
+
+            'Ribbon.ribbonref.InvalidateRibbon()
+
+        Catch ex As Exception
+            'ErrorHandler.DisplayMessage(ex)
+
+        End Try
+
+    End Sub
 
 End Class
