f1cfe6960dd1965b38a09539f7021ca2584db9b3
tduguid <tduguid@NB61300907>
2018-11-19 11:51:27 +1100

    Updated error trap in settings taskpane

--- CS/TaskPane/Settings.cs
+++ CS/TaskPane/Settings.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows.Forms;
 using System.Reflection;
+using ScriptHelp.Scripts;
 
 namespace ScriptHelp.TaskPane
 {
@@ -15,8 +16,15 @@ namespace ScriptHelp.TaskPane
         /// </summary>
         public Settings()
         {
-            InitializeComponent();
-            this.pgdSettings.SelectedObject = Properties.Settings.Default;
+            try
+            {
+                InitializeComponent();
+                this.pgdSettings.SelectedObject = Properties.Settings.Default;
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+            }
         }
 
         /// <summary> 
@@ -27,21 +35,28 @@ namespace ScriptHelp.TaskPane
         /// <remarks></remarks>
         public static void SetLabelColumnWidth(PropertyGrid grid, int width)
         {
-            if (grid == null)
-                return;
+            try
+            {
+                if (grid == null)
+                    return;
 
-            FieldInfo fi = grid.GetType().GetField("gridView", BindingFlags.Instance | BindingFlags.NonPublic);
-            if (fi == null)
-                return;
+                FieldInfo fi = grid.GetType().GetField("gridView", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (fi == null)
+                    return;
 
-            Control view = fi.GetValue(grid) as Control;
-            if (view == null)
-                return;
+                Control view = fi.GetValue(grid) as Control;
+                if (view == null)
+                    return;
 
-            MethodInfo mi = view.GetType().GetMethod("MoveSplitterTo", BindingFlags.Instance | BindingFlags.NonPublic);
-            if (mi == null)
-                return;
-            mi.Invoke(view, new object[] { width });
+                MethodInfo mi = view.GetType().GetMethod("MoveSplitterTo", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (mi == null)
+                    return;
+                mi.Invoke(view, new object[] { width });
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+            }
         }
 
         /// <summary>
@@ -51,9 +66,16 @@ namespace ScriptHelp.TaskPane
         /// <param name="e">refers to the event arguments for the used event, they usually come in the form of properties/functions/methods that get to be available on it.</param>
         private void pgdSettings_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
         {
-            Properties.Settings.Default.Save();
-            Scripts.Ribbon.ribbonref.InvalidateRibbon();
+            try
+            {
+                Properties.Settings.Default.Save();
+                Scripts.Ribbon.ribbonref.InvalidateRibbon();
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+            }
         }
-        
+
     }
 }
